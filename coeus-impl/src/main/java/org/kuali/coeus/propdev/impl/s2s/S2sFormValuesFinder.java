126f0d5cc7eaf67119569d664677299441590984
Travis Schneeberger <leo.herbie@gmail.com>
2017-04-25 08:51:42 -0400

    RESKC-1459: adding maintenance table for s2s form configuration.

---
+++ coeus-impl/src/main/java/org/kuali/coeus/propdev/impl/s2s/S2sFormValuesFinder.java
@@ -0,0 +1,41 @@
+package org.kuali.coeus.propdev.impl.s2s;
+
+import org.kuali.coeus.s2sgen.api.generate.FormMappingInfo;
+import org.kuali.coeus.s2sgen.api.generate.FormMappingService;
+import org.kuali.coeus.sys.framework.service.KcServiceLocator;
+import org.kuali.rice.core.api.util.ConcreteKeyValue;
+import org.kuali.rice.core.api.util.KeyValue;
+import org.kuali.rice.krad.uif.control.UifKeyValuesFinderBase;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class S2sFormValuesFinder extends UifKeyValuesFinderBase {
+
+    private transient FormMappingService formMappingService;
+    private static final ConcreteKeyValue SELECT = new ConcreteKeyValue("", "select");
+
+    @Override
+    public List<KeyValue> getKeyValues() {
+        return Stream.concat(Stream.of(SELECT), getFormMappingService().getBindings()
+                .values()
+                .stream()
+                .map(FormMappingInfo::getFormName)
+                .sorted(String.CASE_INSENSITIVE_ORDER)
+                .map(name -> new ConcreteKeyValue(name, name)))
+                .collect(Collectors.toList());
+    }
+
+    public FormMappingService getFormMappingService() {
+        if (formMappingService == null) {
+            formMappingService = KcServiceLocator.getService(FormMappingService.class);
+        }
+
+        return formMappingService;
+    }
+
+    public void setFormMappingService(FormMappingService formMappingService) {
+        this.formMappingService = formMappingService;
+    }
+}
