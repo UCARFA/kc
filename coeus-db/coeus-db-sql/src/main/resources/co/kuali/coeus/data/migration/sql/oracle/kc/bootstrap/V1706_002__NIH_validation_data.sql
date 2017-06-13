57fbcfd6b003c4c52341aa8a1205a1b0094f817b
Douglas Pace <douglas.pace@gmail.com>
2017-06-13 13:12:55 -0600

    RESKC-2300: Fix Oracle sql scripts (#2386)

--- coeus-db/coeus-db-sql/src/main/resources/co/kuali/coeus/data/migration/sql/oracle/kc/bootstrap/V1706_002__NIH_validation_data.sql
+++ coeus-db/coeus-db-sql/src/main/resources/co/kuali/coeus/data/migration/sql/oracle/kc/bootstrap/V1706_002__NIH_validation_data.sql
@@ -19,170 +19,167 @@
 
 alter table NIH_VALIDATION_MAPPING modify (PAGE_ID null);
 
-insert into SEQ_NIH_VALIDATION values (null);
 insert into NIH_VALIDATION_MAPPING (ID, RULE_NUMBER, CUSTOM_MSG, FORCE_ERROR, APPEND_TO_ORIGINAL, PAGE_ID,
 SECTION_ID, ACTIVE, UPDATE_USER, UPDATE_TIMESTAMP, VER_NBR, OBJ_ID) values
-((select max(ID) from SEQ_NIH_VALIDATION), '0.3', 'Please correct the DUNS number on the submitting organization maintenace record',
+(SEQ_NIH_VALIDATION.nextval, '0.3', 'Please correct the DUNS number on the submitting organization maintenace record',
 '0', '1', '', '', '1', 'admin', SYSDATE, '1', SYS_GUID());
 
-insert into SEQ_NIH_VALIDATION values (null);
 insert into NIH_VALIDATION_MAPPING (ID, RULE_NUMBER, CUSTOM_MSG, FORCE_ERROR, APPEND_TO_ORIGINAL, PAGE_ID,
 SECTION_ID, ACTIVE, UPDATE_USER, UPDATE_TIMESTAMP, VER_NBR, OBJ_ID) values
-((select max(ID) from SEQ_NIH_VALIDATION), '0.11', '',
+(SEQ_NIH_VALIDATION.nextval, '0.11', '',
 '0', '1', 'PropDev-AttachmentsPage', 'PropDev-AttachmentsPage-ProposalSection', '1', 'admin', SYSDATE, '1', SYS_GUID());
 
-insert into SEQ_NIH_VALIDATION values (null);
 insert into NIH_VALIDATION_MAPPING (ID, RULE_NUMBER, CUSTOM_MSG, FORCE_ERROR, APPEND_TO_ORIGINAL, PAGE_ID,
 SECTION_ID, ACTIVE, UPDATE_USER, UPDATE_TIMESTAMP, VER_NBR, OBJ_ID) values
-((select max(ID) from SEQ_NIH_VALIDATION), '0.27', '',
+(SEQ_NIH_VALIDATION.nextval, '0.27', '',
 '0', '1', 'PropDev-OpportunityPage', 'PropDev-OpportunityPage-Forms', '1', 'admin', SYSDATE, '1', SYS_GUID());
 
-insert into SEQ_NIH_VALIDATION values (null);
+
 insert into NIH_VALIDATION_MAPPING (ID, RULE_NUMBER, CUSTOM_MSG, FORCE_ERROR, APPEND_TO_ORIGINAL, PAGE_ID,
 SECTION_ID, ACTIVE, UPDATE_USER, UPDATE_TIMESTAMP, VER_NBR, OBJ_ID) values
-((select max(ID) from SEQ_NIH_VALIDATION), '0.28', '',
+(SEQ_NIH_VALIDATION.nextval, '0.28', '',
 '0', '1', 'PropDev-OpportunityPage', 'PropDev-OpportunityPage-Forms', '1', 'admin', SYSDATE, '1', SYS_GUID());
 
-insert into SEQ_NIH_VALIDATION values (null);
+
 insert into NIH_VALIDATION_MAPPING (ID, RULE_NUMBER, CUSTOM_MSG, FORCE_ERROR, APPEND_TO_ORIGINAL, PAGE_ID,
 SECTION_ID, ACTIVE, UPDATE_USER, UPDATE_TIMESTAMP, VER_NBR, OBJ_ID) values
-((select max(ID) from SEQ_NIH_VALIDATION), '0.31', 'Your submitting institution will take action on the proposers behalf. No action is necessary.',
+(SEQ_NIH_VALIDATION.nextval, '0.31', 'Your submitting institution will take action on the proposers behalf. No action is necessary.',
 '0', '1', '', '', '1', 'admin', SYSDATE, '1', SYS_GUID());
 
-insert into SEQ_NIH_VALIDATION values (null);
+
 insert into NIH_VALIDATION_MAPPING (ID, RULE_NUMBER, CUSTOM_MSG, FORCE_ERROR, APPEND_TO_ORIGINAL, PAGE_ID,
 SECTION_ID, ACTIVE, UPDATE_USER, UPDATE_TIMESTAMP, VER_NBR, OBJ_ID) values
-((select max(ID) from SEQ_NIH_VALIDATION), '0.38', 'Please confirm that your selection of funding opportunity announcement is correct.',
+(SEQ_NIH_VALIDATION.nextval, '0.38', 'Please confirm that your selection of funding opportunity announcement is correct.',
 '0', '1', 'PropDev-OpportunityPage', '', '1', 'admin', SYSDATE, '1', SYS_GUID());
 
-insert into SEQ_NIH_VALIDATION values (null);
+
 insert into NIH_VALIDATION_MAPPING (ID, RULE_NUMBER, CUSTOM_MSG, FORCE_ERROR, APPEND_TO_ORIGINAL, PAGE_ID,
 SECTION_ID, ACTIVE, UPDATE_USER, UPDATE_TIMESTAMP, VER_NBR, OBJ_ID) values
-((select max(ID) from SEQ_NIH_VALIDATION), '0.37', '',
+(SEQ_NIH_VALIDATION.nextval, '0.37', '',
 '0', '1', 'PropDev-AttachmentsPage', 'PropDev-AttachmentsPage-ProposalSection', '1', 'admin', SYSDATE, '1', SYS_GUID());
 
-insert into SEQ_NIH_VALIDATION values (null);
+
 insert into NIH_VALIDATION_MAPPING (ID, RULE_NUMBER, CUSTOM_MSG, FORCE_ERROR, APPEND_TO_ORIGINAL, PAGE_ID,
 SECTION_ID, ACTIVE, UPDATE_USER, UPDATE_TIMESTAMP, VER_NBR, OBJ_ID) values
-((select max(ID) from SEQ_NIH_VALIDATION), '0.8', '',
+(SEQ_NIH_VALIDATION.nextval, '0.8', '',
 '0', '1', 'PropDev-AttachmentsPage', 'PropDev-AttachmentsPage-ProposalSection', '1', 'admin', SYSDATE, '1', SYS_GUID());
 
-insert into SEQ_NIH_VALIDATION values (null);
+
 insert into NIH_VALIDATION_MAPPING (ID, RULE_NUMBER, CUSTOM_MSG, FORCE_ERROR, APPEND_TO_ORIGINAL, PAGE_ID,
 SECTION_ID, ACTIVE, UPDATE_USER, UPDATE_TIMESTAMP, VER_NBR, OBJ_ID) values
-((select max(ID) from SEQ_NIH_VALIDATION), '0.9', '',
+(SEQ_NIH_VALIDATION.nextval, '0.9', '',
 '0', '1', 'PropDev-AttachmentsPage', 'PropDev-AttachmentsPage-ProposalSection', '1', 'admin', SYSDATE, '1', SYS_GUID());
 
-insert into SEQ_NIH_VALIDATION values (null);
+
 insert into NIH_VALIDATION_MAPPING (ID, RULE_NUMBER, CUSTOM_MSG, FORCE_ERROR, APPEND_TO_ORIGINAL, PAGE_ID,
 SECTION_ID, ACTIVE, UPDATE_USER, UPDATE_TIMESTAMP, VER_NBR, OBJ_ID) values
-((select max(ID) from SEQ_NIH_VALIDATION), '0.1', '',
+(SEQ_NIH_VALIDATION.nextval, '0.1', '',
 '0', '1', 'PropDev-AttachmentsPage', 'PropDev-AttachmentsPage-ProposalSection', '1', 'admin', SYSDATE, '1', SYS_GUID());
 
-insert into SEQ_NIH_VALIDATION values (null);
+
 insert into NIH_VALIDATION_MAPPING (ID, RULE_NUMBER, CUSTOM_MSG, FORCE_ERROR, APPEND_TO_ORIGINAL, PAGE_ID,
 SECTION_ID, ACTIVE, UPDATE_USER, UPDATE_TIMESTAMP, VER_NBR, OBJ_ID) values
-((select max(ID) from SEQ_NIH_VALIDATION), '0.13', '',
+(SEQ_NIH_VALIDATION.nextval, '0.13', '',
 '0', '1', 'PropDev-AttachmentsPage', 'PropDev-AttachmentsPage-ProposalSection', '1', 'admin', SYSDATE, '1', SYS_GUID());
 
-insert into SEQ_NIH_VALIDATION values (null);
+
 insert into NIH_VALIDATION_MAPPING (ID, RULE_NUMBER, CUSTOM_MSG, FORCE_ERROR, APPEND_TO_ORIGINAL, PAGE_ID,
 SECTION_ID, ACTIVE, UPDATE_USER, UPDATE_TIMESTAMP, VER_NBR, OBJ_ID) values
-((select max(ID) from SEQ_NIH_VALIDATION), '0.2', '',
+(SEQ_NIH_VALIDATION.nextval, '0.2', '',
 '0', '1', 'PropDev-AttachmentsPage', 'PropDev-AttachmentsPage-ProposalSection', '1', 'admin', SYSDATE, '1', SYS_GUID());
 
-insert into SEQ_NIH_VALIDATION values (null);
+
 insert into NIH_VALIDATION_MAPPING (ID, RULE_NUMBER, CUSTOM_MSG, FORCE_ERROR, APPEND_TO_ORIGINAL, PAGE_ID,
 SECTION_ID, ACTIVE, UPDATE_USER, UPDATE_TIMESTAMP, VER_NBR, OBJ_ID) values
-((select max(ID) from SEQ_NIH_VALIDATION), '0.21', '',
+(SEQ_NIH_VALIDATION.nextval, '0.21', '',
 '0', '1', 'PropDev-AttachmentsPage', 'PropDev-AttachmentsPage-ProposalSection', '1', 'admin', SYSDATE, '1', SYS_GUID());
 
-insert into SEQ_NIH_VALIDATION values (null);
+
 insert into NIH_VALIDATION_MAPPING (ID, RULE_NUMBER, CUSTOM_MSG, FORCE_ERROR, APPEND_TO_ORIGINAL, PAGE_ID,
 SECTION_ID, ACTIVE, UPDATE_USER, UPDATE_TIMESTAMP, VER_NBR, OBJ_ID) values
-((select max(ID) from SEQ_NIH_VALIDATION), '0.23', '',
+(SEQ_NIH_VALIDATION.nextval, '0.23', '',
 '0', '1', 'PropDev-AttachmentsPage', 'PropDev-AttachmentsPage-PersonnelSection', '1', 'admin', SYSDATE, '1', SYS_GUID());
 
-insert into SEQ_NIH_VALIDATION values (null);
+
 insert into NIH_VALIDATION_MAPPING (ID, RULE_NUMBER, CUSTOM_MSG, FORCE_ERROR, APPEND_TO_ORIGINAL, PAGE_ID,
 SECTION_ID, ACTIVE, UPDATE_USER, UPDATE_TIMESTAMP, VER_NBR, OBJ_ID) values
-((select max(ID) from SEQ_NIH_VALIDATION), '0.25', '',
+(SEQ_NIH_VALIDATION.nextval, '0.25', '',
 '0', '1', 'PropDev-AttachmentsPage', 'PropDev-AttachmentsPage-ProposalSection', '1', 'admin', SYSDATE, '1', SYS_GUID());
 
-insert into SEQ_NIH_VALIDATION values (null);
+
 insert into NIH_VALIDATION_MAPPING (ID, RULE_NUMBER, CUSTOM_MSG, FORCE_ERROR, APPEND_TO_ORIGINAL, PAGE_ID,
 SECTION_ID, ACTIVE, UPDATE_USER, UPDATE_TIMESTAMP, VER_NBR, OBJ_ID) values
-((select max(ID) from SEQ_NIH_VALIDATION), '0.26', '',
+(SEQ_NIH_VALIDATION.nextval, '0.26', '',
 '0', '1', 'PropDev-AttachmentsPage', 'PropDev-AttachmentsPage-ProposalSection', '1', 'admin', SYSDATE, '1', SYS_GUID());
 
-insert into SEQ_NIH_VALIDATION values (null);
+
 insert into NIH_VALIDATION_MAPPING (ID, RULE_NUMBER, CUSTOM_MSG, FORCE_ERROR, APPEND_TO_ORIGINAL, PAGE_ID,
 SECTION_ID, ACTIVE, UPDATE_USER, UPDATE_TIMESTAMP, VER_NBR, OBJ_ID) values
-((select max(ID) from SEQ_NIH_VALIDATION), '0.34', '',
+(SEQ_NIH_VALIDATION.nextval, '0.34', '',
 '0', '1', 'PropDev-AttachmentsPage', 'PropDev-AttachmentsPage-ProposalSection', '1', 'admin', SYSDATE, '1', SYS_GUID());
 
-insert into SEQ_NIH_VALIDATION values (null);
+
 insert into NIH_VALIDATION_MAPPING (ID, RULE_NUMBER, CUSTOM_MSG, FORCE_ERROR, APPEND_TO_ORIGINAL, PAGE_ID,
 SECTION_ID, ACTIVE, UPDATE_USER, UPDATE_TIMESTAMP, VER_NBR, OBJ_ID) values
-((select max(ID) from SEQ_NIH_VALIDATION), '0.35', '',
+(SEQ_NIH_VALIDATION.nextval, '0.35', '',
 '0', '1', 'PropDev-AttachmentsPage', 'PropDev-AttachmentsPage-ProposalSection', '1', 'admin', SYSDATE, '1', SYS_GUID());
 
-insert into SEQ_NIH_VALIDATION values (null);
+
 insert into NIH_VALIDATION_MAPPING (ID, RULE_NUMBER, CUSTOM_MSG, FORCE_ERROR, APPEND_TO_ORIGINAL, PAGE_ID,
 SECTION_ID, ACTIVE, UPDATE_USER, UPDATE_TIMESTAMP, VER_NBR, OBJ_ID) values
-((select max(ID) from SEQ_NIH_VALIDATION), '01.1.1', '',
+(SEQ_NIH_VALIDATION.nextval, '01.1.1', '',
 '0', '1', 'PropDev-DetailsPage', 'PropDev-DetailsPage-Section', '1', 'admin', SYSDATE, '1', SYS_GUID());
 
-insert into SEQ_NIH_VALIDATION values (null);
+
 insert into NIH_VALIDATION_MAPPING (ID, RULE_NUMBER, CUSTOM_MSG, FORCE_ERROR, APPEND_TO_ORIGINAL, PAGE_ID,
 SECTION_ID, ACTIVE, UPDATE_USER, UPDATE_TIMESTAMP, VER_NBR, OBJ_ID) values
-((select max(ID) from SEQ_NIH_VALIDATION), '001.1.2', '',
+(SEQ_NIH_VALIDATION.nextval, '001.1.2', '',
 '0', '1', 'PropDev-OpportunityPage', 'PropDev-OpportunityPage-General', '1', 'admin', SYSDATE, '1', SYS_GUID());
 
-insert into SEQ_NIH_VALIDATION values (null);
+
 insert into NIH_VALIDATION_MAPPING (ID, RULE_NUMBER, CUSTOM_MSG, FORCE_ERROR, APPEND_TO_ORIGINAL, PAGE_ID,
 SECTION_ID, ACTIVE, UPDATE_USER, UPDATE_TIMESTAMP, VER_NBR, OBJ_ID) values
-((select max(ID) from SEQ_NIH_VALIDATION), '001.1.3', 'Please review the Sponsor Proposal number and or Previous Grants.gov number .',
+(SEQ_NIH_VALIDATION.nextval, '001.1.3', 'Please review the Sponsor Proposal number and or Previous Grants.gov number .',
 '0', '1', '', '', '1', 'admin', SYSDATE, '1', SYS_GUID());
 
-insert into SEQ_NIH_VALIDATION values (null);
+
 insert into NIH_VALIDATION_MAPPING (ID, RULE_NUMBER, CUSTOM_MSG, FORCE_ERROR, APPEND_TO_ORIGINAL, PAGE_ID,
 SECTION_ID, ACTIVE, UPDATE_USER, UPDATE_TIMESTAMP, VER_NBR, OBJ_ID) values
-((select max(ID) from SEQ_NIH_VALIDATION), '001.1.5', '',
+(SEQ_NIH_VALIDATION.nextval, '001.1.5', '',
 '0', '1', 'PropDev-OpportunityPage', 'PropDev-OpportunityPage-General', '1', 'admin', SYSDATE, '1', SYS_GUID());
 
-insert into SEQ_NIH_VALIDATION values (null);
+
 insert into NIH_VALIDATION_MAPPING (ID, RULE_NUMBER, CUSTOM_MSG, FORCE_ERROR, APPEND_TO_ORIGINAL, PAGE_ID,
 SECTION_ID, ACTIVE, UPDATE_USER, UPDATE_TIMESTAMP, VER_NBR, OBJ_ID) values
-((select max(ID) from SEQ_NIH_VALIDATION), '001.1.6', '',
+(SEQ_NIH_VALIDATION.nextval, '001.1.6', '',
 '0', '1', 'PropDev-DetailsPage', 'PropDev-DetailsPage-Section', '1', 'admin', SYSDATE, '1', SYS_GUID());
 
-insert into SEQ_NIH_VALIDATION values (null);
+
 insert into NIH_VALIDATION_MAPPING (ID, RULE_NUMBER, CUSTOM_MSG, FORCE_ERROR, APPEND_TO_ORIGINAL, PAGE_ID,
 SECTION_ID, ACTIVE, UPDATE_USER, UPDATE_TIMESTAMP, VER_NBR, OBJ_ID) values
-((select max(ID) from SEQ_NIH_VALIDATION), '001.1.4', '',
+(SEQ_NIH_VALIDATION.nextval, '001.1.4', '',
 '0', '1', 'PropDev-SponsorProgramInfoPage', '', '1', 'admin', SYSDATE, '1', SYS_GUID());
 
-insert into SEQ_NIH_VALIDATION values (null);
+
 insert into NIH_VALIDATION_MAPPING (ID, RULE_NUMBER, CUSTOM_MSG, FORCE_ERROR, APPEND_TO_ORIGINAL, PAGE_ID,
 SECTION_ID, ACTIVE, UPDATE_USER, UPDATE_TIMESTAMP, VER_NBR, OBJ_ID) values
-((select max(ID) from SEQ_NIH_VALIDATION), '001.1.6.2', '',
+(SEQ_NIH_VALIDATION.nextval, '001.1.6.2', '',
 '0', '1', 'PropDev-SponsorProgramInfoPage', 'Sponsor Proposal ID', '1', 'admin', SYSDATE, '1', SYS_GUID());
 
-insert into SEQ_NIH_VALIDATION values (null);
+
 insert into NIH_VALIDATION_MAPPING (ID, RULE_NUMBER, CUSTOM_MSG, FORCE_ERROR, APPEND_TO_ORIGINAL, PAGE_ID,
 SECTION_ID, ACTIVE, UPDATE_USER, UPDATE_TIMESTAMP, VER_NBR, OBJ_ID) values
-((select max(ID) from SEQ_NIH_VALIDATION), '001.1.6.3', '',
+(SEQ_NIH_VALIDATION.nextval, '001.1.6.3', '',
 '0', '1', 'PropDev-SponsorProgramInfoPage', 'Sponsor Proposal ID', '1', 'admin', SYSDATE, '1', SYS_GUID());
 
-insert into SEQ_NIH_VALIDATION values (null);
+
 insert into NIH_VALIDATION_MAPPING (ID, RULE_NUMBER, CUSTOM_MSG, FORCE_ERROR, APPEND_TO_ORIGINAL, PAGE_ID,
 SECTION_ID, ACTIVE, UPDATE_USER, UPDATE_TIMESTAMP, VER_NBR, OBJ_ID) values
-((select max(ID) from SEQ_NIH_VALIDATION), '001.1.6.4', '',
+(SEQ_NIH_VALIDATION.nextval, '001.1.6.4', '',
 '0', '1', 'PropDev-SponsorProgramInfoPage', 'Sponsor Proposal ID', '1', 'admin', SYSDATE, '1', SYS_GUID());
 
-insert into SEQ_NIH_VALIDATION values (null);
+
 insert into NIH_VALIDATION_MAPPING (ID, RULE_NUMBER, CUSTOM_MSG, FORCE_ERROR, APPEND_TO_ORIGINAL, PAGE_ID,
 SECTION_ID, ACTIVE, UPDATE_USER, UPDATE_TIMESTAMP, VER_NBR, OBJ_ID) values
-((select max(ID) from SEQ_NIH_VALIDATION), '001.6.5', 'If the Sponsor Proposal ID listed is correct, check the Questionnaire for Cover Page Supplement and the Key Personnel to ensure that the PD/PI listed is correct.',
+(SEQ_NIH_VALIDATION.nextval, '001.6.5', 'If the Sponsor Proposal ID listed is correct, check the Questionnaire for Cover Page Supplement and the Key Personnel to ensure that the PD/PI listed is correct.',
 '0', '1', 'PropDev-SponsorProgramInfoPage', '', '1', 'admin', SYSDATE, '1', SYS_GUID());
