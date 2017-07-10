6171c1fc6ddce57dd3d73080c973d00502cd16fe
Terry Durkin <terry@kuali.co>
2017-07-10 17:23:02 -0400

    RESOPS-1639: Add indexes to improve API performance (#2421)

---
+++ coeus-db/coeus-db-sql/src/main/resources/co/kuali/coeus/data/migration/sql/mysql/kc/bootstrap/V1707_004__award_posts_indexes.sql
@@ -0,0 +1,21 @@
+--
+-- Kuali Coeus, a comprehensive research administration system for higher education.
+--
+-- Copyright 2005-2016 Kuali, Inc.
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU Affero General Public License as
+-- published by the Free Software Foundation, either version 3 of the
+-- License, or (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU Affero General Public License for more details.
+--
+-- You should have received a copy of the GNU Affero General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--
+
+create index AWARD_SPONSOR_TERM_ID_IDX on AWARD_SPONSOR_TERM (AWARD_ID);
+create index VERS_HIST_SEQ_OWNER_NM_NBR_IDX on VERSION_HISTORY (SEQ_OWNER_VERSION_NAME_VALUE, SEQ_OWNER_SEQ_NUMBER);
