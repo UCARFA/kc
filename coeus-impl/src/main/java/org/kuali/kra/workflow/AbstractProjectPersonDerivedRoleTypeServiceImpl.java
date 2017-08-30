c4ced5bfdeaa636b652e039bef9bd2a4ba74312f
fait@ucar.edu <Sunshine0fYourLove>
2017-08-30 09:26:25 -0600

    Added change to derived role for unit contacts

--- coeus-impl/src/main/java/org/kuali/kra/workflow/AbstractProjectPersonDerivedRoleTypeServiceImpl.java
+++ coeus-impl/src/main/java/org/kuali/kra/workflow/AbstractProjectPersonDerivedRoleTypeServiceImpl.java
@@ -19,6 +19,7 @@
 package org.kuali.kra.workflow;
 
 import org.apache.commons.lang3.StringUtils;
+import org.kuali.kra.award.contacts.AwardUnitContact;
 import org.kuali.kra.bo.AbstractProjectPerson;
 import org.kuali.kra.infrastructure.Constants;
 import org.kuali.kra.kim.bo.KcKimAttributes;
@@ -33,6 +34,8 @@ import java.util.Map;
 public abstract class AbstractProjectPersonDerivedRoleTypeServiceImpl extends DerivedRoleTypeServiceBase {
     
     protected abstract List<? extends AbstractProjectPerson> getProjectPersons(Map<String, String> qualification);
+    // KTW - Add unit contacts
+    protected abstract List<? extends AwardUnitContact> getProjectUnitContacts(Map<String, String> qualification);
     
     /**
      * Filter the list of negotiation persons by their role. Typically the role name
@@ -59,6 +62,10 @@ public abstract class AbstractProjectPersonDerivedRoleTypeServiceImpl extends De
 
         List<RoleMembership> members = new ArrayList<RoleMembership>();
         List<? extends AbstractProjectPerson> projectPersons = getProjectPersons(qualification);
+        // KTW - Add unit contacts also
+        List<? extends AwardUnitContact> projectUnitContacts = getProjectUnitContacts(qualification);
+
+
         String subQualification = qualification.get(KcKimAttributes.SUB_QUALIFIER);
         
         if (projectPersons != null && !projectPersons.isEmpty()) {
@@ -73,6 +80,12 @@ public abstract class AbstractProjectPersonDerivedRoleTypeServiceImpl extends De
             for (AbstractProjectPerson proposalPerson : projectPersons) {
                 if (proposalPerson.getPerson() != null) {
                     members.add( RoleMembership.Builder.create(null, null, proposalPerson.getPerson().getPersonId(), MemberType.PRINCIPAL, null).build() );
+                }
+            }
+            // KTW - add unit contacts to role
+            for (AwardUnitContact unitContact : projectUnitContacts) {
+                if (unitContact.getPerson() != null) {
+                    members.add( RoleMembership.Builder.create(null, null, unitContact.getPerson().getPersonId(), MemberType.PRINCIPAL, null).build() );
                 }
             }
         }
