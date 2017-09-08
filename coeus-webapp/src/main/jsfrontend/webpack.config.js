f4081dfed02bac53df3fd3716a390d6872430e95
Travis Schneeberger <leo.herbie@gmail.com>
2017-09-08 15:07:22 -0400

    Increase build performance for budget rates maintenance

--- coeus-webapp/src/main/jsfrontend/webpack.config.js
+++ coeus-webapp/src/main/jsfrontend/webpack.config.js
@@ -33,8 +33,8 @@ module.exports = {
 	},
 	module: {
 		loaders: [
-			{test: /\.js/, loader: 'babel-loader'},
-			{test: /\.jsx/, loader: 'babel-loader'}
+			{test: /\.js/, loader: 'babel-loader', 'exclude': /node_modules/},
+			{test: /\.jsx/, loader: 'babel-loader', 'exclude': /node_modules/}
 		]
 	}
 };
