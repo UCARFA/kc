2b829f6b8192eef68b4041946cb9db5c11c26174
Steve Waltman <stevew@ucar.edu>
2017-11-20 15:34:53 -0700

    Add functionality for PIs to search for IPs

---
+++ coeus-impl/src/main/java/edu/ucar/fanda/kuali/kra/institutionalproposal/document/authorization/UcarInstitutionalProposalDocumentAuthorizer.java
@@ -0,0 +1,36 @@
+package edu.ucar.fanda.kuali.kra.institutionalproposal.document.authorization;
+
+import org.kuali.kra.institutionalproposal.document.InstitutionalProposalDocument;
+import org.kuali.kra.institutionalproposal.document.authorization.InstitutionalProposalDocumentAuthorizer;
+import org.kuali.kra.kim.bo.KcKimAttributes;
+
+import java.util.Map;
+
+public class UcarInstitutionalProposalDocumentAuthorizer extends InstitutionalProposalDocumentAuthorizer {
+
+    @Override
+    protected void addRoleQualification(
+            Object primaryBusinessObjectOrDocument,
+            Map<String, String> attributes) {
+        System.out.println("UcarInstitutionalProposalDocumentAuthorizer::addRoleQualification");
+        super.addRoleQualification(primaryBusinessObjectOrDocument, attributes);
+        InstitutionalProposalDocument institutionalProposalDocument = (InstitutionalProposalDocument) primaryBusinessObjectOrDocument;
+        if (institutionalProposalDocument.getInstitutionalProposal() != null
+                && institutionalProposalDocument.getInstitutionalProposal().getLeadUnit() != null) {
+            attributes.put(KcKimAttributes.UNIT_NUMBER, institutionalProposalDocument.getInstitutionalProposal().getLeadUnit().getUnitNumber());
+            // KTW - add proposal id if there is one.
+            if (  institutionalProposalDocument.getInstitutionalProposal().getProposalId() != null){
+                attributes.put(KcKimAttributes.PROPOSAL,
+                        institutionalProposalDocument
+                                .getInstitutionalProposal().getProposalId().toString());
+            }
+        } else {
+            attributes.put(KcKimAttributes.UNIT_NUMBER, "*");
+            // KTW
+            if (institutionalProposalDocument.getInstitutionalProposal() != null) {
+                attributes.put(KcKimAttributes.PROPOSAL,
+                        institutionalProposalDocument.getInstitutionalProposal().getProposalId().toString());
+            }
+        }
+    }
+}
---
+++ coeus-impl/src/main/java/edu/ucar/fanda/kuali/kra/institutionalproposal/service/impl/UcarInstitutionalProposalLookupableHelperServiceImpl.java
@@ -0,0 +1,28 @@
+package edu.ucar.fanda.kuali.kra.institutionalproposal.service.impl;
+
+import org.kuali.coeus.sys.framework.util.CollectionUtils;
+import org.kuali.kra.institutionalproposal.document.authorization.InstitutionalProposalDocumentAuthorizer;
+import org.kuali.kra.institutionalproposal.home.InstitutionalProposal;
+import org.kuali.kra.institutionalproposal.service.impl.InstitutionalProposalLookupableHelperServiceImpl;
+import org.kuali.rice.kim.api.identity.Person;
+import org.kuali.rice.krad.util.GlobalVariables;
+import edu.ucar.fanda.kuali.kra.institutionalproposal.document.authorization.UcarInstitutionalProposalDocumentAuthorizer;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class UcarInstitutionalProposalLookupableHelperServiceImpl extends InstitutionalProposalLookupableHelperServiceImpl {
+
+    /**
+     * This method filters results so that the person doing the lookup only gets back the documents he can view.
+     */
+    @Override
+    protected List<InstitutionalProposal> filterForPermissions(List<InstitutionalProposal> results) {
+        System.out.println("UcarInstitutionalProposalLookupableHelperServiceImpl::filterForPermissions");
+        Person user = GlobalVariables.getUserSession().getPerson();
+        UcarInstitutionalProposalDocumentAuthorizer authorizer = new UcarInstitutionalProposalDocumentAuthorizer();
+        List<InstitutionalProposal> filteredResults = CollectionUtils.createCorrectImplementationForCollection(results);
+        filteredResults.addAll(results.stream().filter(institutionalProposal -> authorizer.canOpen(institutionalProposal.getInstitutionalProposalDocument(), user)).collect(Collectors.toList()));
+
+        return filteredResults;
+    }
+}
