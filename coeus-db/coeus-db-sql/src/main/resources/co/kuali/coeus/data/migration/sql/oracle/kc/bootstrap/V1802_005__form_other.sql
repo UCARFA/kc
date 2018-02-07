778aa652272d2df403cc4883f4bbf822c3a7e8c8
Travis Schneeberger <leo.herbie@gmail.com>
2018-02-07 16:35:30 -0500

    RESKC-2724: support Other form 1.2

---
+++ coeus-db/coeus-db-sql/src/main/resources/co/kuali/coeus/data/migration/sql/oracle/kc/bootstrap/V1802_005__form_other.sql
@@ -0,0 +1,12 @@
+--
+-- Copyright © 2005-2018 Kuali, Inc.
+-- All Rights Reserved
+-- You may use and modify this code under the terms of the Kuali, Inc.
+-- Pre-Release License Agreement. You may not distribute it.
+--
+-- You should have received a copy of the Kuali, Inc. Pre-Release License
+-- Agreement with this file. If not, please write to license@kuali.co.
+--
+
+INSERT INTO VALID_NARR_FORMS (VALID_NARR_FORMS_ID,FORM_NAME,NARRATIVE_TYPE_CODE,MANDATORY,UPDATE_USER,UPDATE_TIMESTAMP,OBJ_ID)
+VALUES (SEQ_VALID_NARR_FORMS_ID.NEXTVAL,'Other-V1.2','19','N','admin',SYSDATE, sys_guid());
