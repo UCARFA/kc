d372d9d42a9146f5fb73c806db389757a49dfe44
Jeff Largent <jeff.largent@kuali.co>
2018-02-15 10:50:17 -0500

    RESKC-2680: Fix typo in parameter value

--- coeus-db/coeus-db-sql/src/main/resources/co/kuali/coeus/data/migration/sql/oracle/rice/bootstrap/V1802_009__art_unit_admin_digest_cron_trigger.sql
+++ coeus-db/coeus-db-sql/src/main/resources/co/kuali/coeus/data/migration/sql/oracle/rice/bootstrap/V1802_009__art_unit_admin_digest_cron_trigger.sql
@@ -12,4 +12,4 @@ insert into krcr_parm_t (nmspc_cd, cmpnt_cd, parm_nm, obj_id, ver_nbr, parm_typ_
 values ('KC-AWARD', 'Document', 'Report_Tracking_UNIT_Digest_Notification_Cron_Trigger', sys_guid(), 1, 'CONFG', '0 59 23 15 * ?', 'Using a quartz cron expression, schedule the frequency of when batch notification overdue digest by UNIT should be sent.', 'A', 'KC');
 
 insert into krcr_parm_t (nmspc_cd, cmpnt_cd, parm_nm, obj_id, ver_nbr, parm_typ_cd, val, parm_desc_txt, eval_oprtr_cd, appl_id)
-values ('KC-AWARD', 'Document', 'Ascend_Unit_Hierarchy_For_Report_Tracking_Digest_Unit_Admins', sys_guid(), 1, 'CONFG', '0 59 23 15 * ?', 'Determines whether or not award report tracking digest notifications should be sent to all Unit Administrators at or above the units associated with reports in the digest. Default value is true.', 'A', 'KC');
+values ('KC-AWARD', 'Document', 'Ascend_Unit_Hierarchy_For_Report_Tracking_Digest_Unit_Admins', sys_guid(), 1, 'CONFG', 'true', 'Determines whether or not award report tracking digest notifications should be sent to all Unit Administrators at or above the units associated with reports in the digest. Default value is true.', 'A', 'KC');
