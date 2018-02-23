3289940adca59fcf8f02f7dd278fb948335940a5
Travis Schneeberger <leo.herbie@gmail.com>
2018-02-23 13:53:21 -0500

    RESKC-2078: update s2s, add tests

---
+++ coeus-it/src/test/java/org/kuali/coeus/s2sgen/impl/generate/support/SF424ShortV1_1GeneratorTest.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright © 2005-2018 Kuali, Inc. - All Rights Reserved
+ * You may use and modify this code under the terms of the Kuali, Inc.
+ * Pre-Release License Agreement. You may not distribute it.
+ *
+ * You should have received a copy of the Kuali, Inc. Pre-Release License
+ * Agreement with this file. If not, please write to license@kuali.co.
+ */
+
+package org.kuali.coeus.s2sgen.impl.generate.support;
+
+public class SF424ShortV1_1GeneratorTest extends SF424ShortBaseGeneratorTest {
+    @Override
+    protected String getFormGeneratorName() {
+        return SF424ShortV1_1Generator.class.getSimpleName();
+    }
+}
