ab2d39f472e11a2ba974eda67f0426be103ef27f
Steve Waltman <stevew@ucar.edu>
2018-05-18 09:12:04 -0600

    Customization for Unit Contact permissions.  Changed award document xml to point to UCAR class.

--- coeus-impl/src/main/java/org/kuali/kra/award/AwardInvestigatorDerivedRoleTypeServiceImpl.java
+++ coeus-impl/src/main/java/org/kuali/kra/award/AwardInvestigatorDerivedRoleTypeServiceImpl.java
@@ -8,11 +8,15 @@
 package org.kuali.kra.award;
 
 import org.apache.commons.lang3.StringUtils;
+import org.kuali.kra.award.contacts.AwardUnitContact;
 import org.kuali.kra.award.home.Award;
 import org.kuali.kra.award.home.AwardService;
 import org.kuali.kra.bo.AbstractProjectPerson;
+import org.kuali.kra.infrastructure.Constants;
 import org.kuali.kra.kim.bo.KcKimAttributes;
 import org.kuali.kra.workflow.AbstractProjectPersonDerivedRoleTypeServiceImpl;
+import org.kuali.rice.core.api.membership.MemberType;
+import org.kuali.rice.kim.api.role.RoleMembership;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -40,6 +44,54 @@ public class AwardInvestigatorDerivedRoleTypeServiceImpl extends AbstractProject
         }
     }
 
+    // UCAR Unit Contacts
+    protected List<? extends AwardUnitContact> getProjectUnitContacts(Map<String, String> qualification) {
+        String awardIdStr = qualification.get(KcKimAttributes.AWARD);
+
+        if (StringUtils.isNotBlank(awardIdStr) && awardIdStr.matches("\\d+")) {
+            Long awardId = Long.valueOf(awardIdStr);
+            Award award = getAwardService().getAward(awardId);
+
+            return award.getAwardUnitContacts();
+        } else {
+            return new ArrayList<AwardUnitContact>();
+        }
+    }
+
+    @Override
+    public List<RoleMembership> getRoleMembersFromDerivedRole(String namespaceCode, String roleName, Map<String,String> qualification) {
+        validateRequiredAttributesAgainstReceived(qualification);
+
+        List<RoleMembership> members = new ArrayList<RoleMembership>();
+        List<? extends AbstractProjectPerson> projectPersons = getProjectPersons(qualification);
+        String subQualification = qualification.get(KcKimAttributes.SUB_QUALIFIER);
+        // UCAR - Add unit contacts
+        List<? extends AwardUnitContact> projectUnitContacts = getProjectUnitContacts(qualification);
+
+        if (projectPersons != null && !projectPersons.isEmpty()) {
+            if (!StringUtils.equals(roleName, Constants.ALL_INVESTIGATORS) && !StringUtils.equals(roleName, Constants.PRINCIPAL_INVESTIGATOR)) {
+                projectPersons = filterListByRole(projectPersons, roleName);
+            }
+
+            if (StringUtils.isNotBlank(subQualification)) {
+                projectPersons = filterListByRole(projectPersons, subQualification);
+            }
+
+            for (AbstractProjectPerson proposalPerson : projectPersons) {
+                if (proposalPerson.getPerson() != null) {
+                    members.add( RoleMembership.Builder.create(null, null, proposalPerson.getPerson().getPersonId(), MemberType.PRINCIPAL, null).build() );
+                }
+            }
+            // UCAR - add unit contacts to role
+            for (AwardUnitContact unitContact : projectUnitContacts) {
+                if (unitContact.getPerson() != null) {
+                    members.add( RoleMembership.Builder.create(null, null, unitContact.getPerson().getPersonId(), MemberType.PRINCIPAL, null).build() );
+                }
+            }
+        }
+
+        return members;
+    }
     protected AwardService getAwardService() {
         return awardService;
     }
